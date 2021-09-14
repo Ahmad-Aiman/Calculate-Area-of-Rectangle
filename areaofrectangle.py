ff33a2eab14bffc1a8c29a9134cabea48b69538b
Ahmad-Aiman <66258284+Ahmad-Aiman@users.noreply.github.com>
2021-09-14 12:13:42 +0800

    Add files via upload

---
+++ areaofrectangle.py
@@ -0,0 +1,14 @@
+#Area of a rectangle = width x length
+#Perimeter of a rectangle = 2 x [length + width#
+
+width_input = float (input("\nPlease enter width: "))
+length_input = float (input("Please enter length: "))
+
+areaofRectangle = width_input * length_input
+
+perimeterofRectangle = 2 * (width_input * length_input)
+
+print ("\nArea of Rectangle is: " , areaofRectangle, "CM")
+
+print("\nPerimeter of Rectangle is: ", perimeterofRectangle, "CM")
+
